f98ae7f638c97aee525b54c32dc6ee7a5d8f5f1f
trustworthyaxolotl26 <efreeman2029@kehillahstudent.org>
2026-02-19 08:40:26 -0800

    proposal

--- my_bot.py
+++ my_bot.py
@@ -1,4 +1,5 @@
-from secret import my_username
+from my_secret import my_username
+import random
 
 """
 **Do NOT change the name of this function.**
@@ -10,7 +11,9 @@ This function will be called every time anyone says anything on a channel where 
 * You can have the bot respond differently to different users
 """
 def should_i_respond(user_message, user_name):
-  if "robot" in user_message:
+  if "fox bot" in user_message:
+    return True
+  elif "robot" in user_message:
     return True
   else:
     return False
@@ -25,5 +28,32 @@ This function will be called every time the `should_i_respond` function returns 
 * You can have the bot respond differently to different messages and users
 """
 def respond(user_message, user_name):
-  return f"""you said my name!!
-  {user_message.replace("robot", user_name)}"""
+  if " number game" in user_message:
+    number_game()
+  elif "best animal" in user_message:
+    return "the best animal is a fox. "
+  elif "ascii fox" in user_message:
+    return """   /\/\                    ____
+< •   \             __/       _]
+  \      \_____/       __/
+   \                       /
+   |     ______     |
+   | |  |            | |  |
+ [_|_|           [_|_|
+"""
+  elif "show me" in user_message:
+    return "not yet."
+  else: 
+    return f"""I've been called!
+    {user_message.replace("fox bot", user_name)}"""
+
+
+def number_game():
+  numberer = input("whould you like to pick a number or should i? ")
+  if numberer == "you pick":
+    fox_num = random.randint(1, 100)
+    return "im thinking of a number 1 - 100"
+
+def new_name():
+  name = input("what shall my new name be?")
+  return f"you may now call me {name}"
